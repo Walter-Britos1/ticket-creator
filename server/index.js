8bbe83ffc10649aba3d45708c9a76cf2b18356be
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-11 21:02:19 -0300

    create database connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import server from './src/server.js';
+import { connectionDataBase } from './src/config/database.js';
 
 const PORT = process.env.PORT || 3000;
+const force = false
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+  await connectionDataBase(force);
   console.log(`Server running in port: ${PORT}`);
 });
