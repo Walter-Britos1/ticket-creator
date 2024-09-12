8e9e49d409ca70c4b8f924e46f1f5bb244e19250
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-11 22:53:57 -0300

    configure route for create new ticket

--- server/src/routes/ticketRoutes.js
+++ server/src/routes/ticketRoutes.js
@@ -1,17 +1,16 @@
 import Route from 'express';
+import createTicketHandler from '../handlers/ticketHandler.js';
 
 const ticketRoute = Route();
 
 ticketRoute.get('/tickets', (req, res) => {
-  res.send('esta ruta mostrara la lista de tickets')
+  res.send('esta ruta mostrara la lista de tickets');
 });
 
-ticketRoute.post('/ticket', (req, res) => {
-  res.send('esta ruta creara un nuevo ticket')
-});
+ticketRoute.post('/ticket', createTicketHandler);
 
 ticketRoute.put('/ticket/:id', (req, res) => {
-  res.send(`esta ruta actualizara el ticket con el id ${req.params.id}`)
+  res.send(`esta ruta actualizara el ticket con el id ${req.params.id}`);
 });
 
 export default ticketRoute;
