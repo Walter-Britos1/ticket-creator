af204e8fd8cba393391fbd550d094a01e9fd0878
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-14 18:44:37 -0300

    Applying functionality to create new tickets

--- client/src/components/TicketForm/TicketForm.jsx
+++ client/src/components/TicketForm/TicketForm.jsx
@@ -1,23 +1,46 @@
+import useTickets from '@/hooks/useTickets';
 
 export default function TicketForm() {
+  const { formData, handleChange, handleSubmit } = useTickets();
+
   return (
     <div>
-      <form className='flex flex-col space-y-4'>
-        <label className='text-sm font-medium'>Subject</label>
-        <input className='w-full px-3 py-2 text-gray-700 border border-gray-300 rounded focus:outline-none focus:border-blue-500' type='text' />
+      <form className='flex flex-col space-y-4' onSubmit={handleSubmit}>
+        <label className='text-sm font-medium'>Name</label>
+        <input 
+          className='w-full px-3 py-2 text-gray-700 border border-gray-300 rounded focus:outline-none focus:border-blue-500' 
+          type='text' 
+          name='name'
+          value={formData.name}
+          onChange={handleChange}
+        />
         <label className='text-sm font-medium'>Description</label>
-        <textarea className='w-full px-3 py-2 text-gray-700 border border-gray-300 rounded focus:outline-none focus:border-blue-500 h-32' rows='5' />
+        <textarea 
+          className='w-full px-3 py-2 text-gray-700 border border-gray-300 rounded focus:outline-none focus:border-blue-500 h-32' 
+          rows='5' 
+          name='description'
+          value={formData.description}
+          onChange={handleChange}
+        />
         <label className='text-sm font-medium'>Difficulty</label>
-
-        {/* Add options for difficulty levels */}
-        <select className='w-full px-3 py-2 text-gray-700 border border-gray-300 rounded focus:outline-none focus:border-blue-500'>
+        <select 
+          className='w-full px-3 py-2 text-gray-700 border border-gray-300 rounded focus:outline-none focus:border-blue-500'
+          name='difficulty'
+          value={formData.difficulty}
+          onChange={handleChange}
+        >
           <option value=''>Select difficulty</option>
           <option value='easy'>Easy</option>
-          <option value='medium'>Medium</option>
+          <option value='intermediate'>Intermediate</option>
           <option value='hard'>Hard</option>
         </select>
         <label className='text-sm font-medium'>Status</label>
-        <select className='w-full px-3 py-2 text-gray-700 border border-gray-300 rounded focus:outline-none focus:border-blue-500'>
+        <select 
+          className='w-full px-3 py-2 text-gray-700 border border-gray-300 rounded focus:outline-none focus:border-blue-500'
+          name='status'
+          value={formData.status}
+          onChange={handleChange}
+        >
           <option value=''>Select status</option>
           <option value='open'>Open</option>
           <option value='in-progress'>In Progress</option>
@@ -26,8 +49,7 @@ export default function TicketForm() {
         <button className='w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition'>
           Submit
         </button>
-        </form>
+      </form>
     </div>
-
-  )
+  );
 }
