ed9aea565b95c08e233e0f2d6a0009045fece06d
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-14 18:43:28 -0300

    adding handler to create ticket

--- client/src/hooks/useTickets.js
+++ client/src/hooks/useTickets.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
+import { useState } from 'react';
 import { setAllTicket } from '@/redux/sliceTicket';
-import { useDispatch, useSelector } from 'react-redux'
-import { GET_ALL_TICKETS_ENDPOINT } from '@/config/api';
- 
+import { useDispatch, useSelector } from 'react-redux';
+import { GET_ALL_TICKETS_ENDPOINT, CREATE_TICKET_ENDPOINT } from '@/config/api';
+import closeModel from '@/hooks/useModal'
+
 const useTickets = () => {
   const dispatch = useDispatch();
-  const tickets = useSelector(state => state.tickets.tickets);
+  const tickets = useSelector((state) => state.tickets.tickets);
 
   const handlerAllTickets = async () => {
     try {
@@ -14,11 +16,50 @@ const useTickets = () => {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-  
+  const handlerCreateTicket = async (formData) => {
+    try {
+      const { data } = await axios.post(CREATE_TICKET_ENDPOINT, formData);
+      dispatch(setAllTicket([...tickets, data]));
+    } catch (error) {
+      console.error('Error creating ticket:', error);
+    }
+  };
 
-  return { tickets, handlerAllTickets };
+
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    difficulty: '',
+    status: '',
+    gifUrl: '',
+    createdAt: new Date(),
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await handlerCreateTicket(formData);
+    setFormData({ name: '', description: '', difficulty: '', status: '' });
+    closeModel()
+  };
+
+  return { 
+    tickets, 
+    formData,
+    handleChange,
+    handleSubmit,
+    handlerAllTickets, 
+    handlerCreateTicket 
+  };
 };
 
 export default useTickets;
