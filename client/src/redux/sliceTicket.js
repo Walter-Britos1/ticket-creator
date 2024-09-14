46be0276008542303a13e2a33665b4f94a6011bd
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-14 00:08:14 -0300

    changing config in setAllTicet

--- client/src/redux/sliceTicket.js
+++ client/src/redux/sliceTicket.js
@@ -1,28 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  tickets: [
-    {
-      id: 'ed98c745-d458-44a7-aef1-542f9346e8ee',
-      name: 'Actualizar pc',
-      description: 'Instalar nuevo sistema operativo',
-      difficulty: 'hard',
-      status: 'resolved',
-      gifUrl: '',
-      createdAt: '2024-09-12T23:21:12.823Z',
-      updatedAt: '2024-09-12T23:24:21.173Z'
-    },
-    {
-      id: '57a6107b-1166-451f-8011-0219befc0a31',
-      name: 'Actualizar telefono',
-      description: 'Instalar nueva version de android',
-      difficulty: 'hard',
-      status: 'in-progress',
-      gifUrl: '',
-      createdAt: '2024-09-13T00:43:52.972Z',
-      updatedAt: '2024-09-13T00:43:52.972Z'
-    }
-  ]
+  tickets: []
 };
 
 const ticketSlice = createSlice({
@@ -30,7 +9,7 @@ const ticketSlice = createSlice({
   initialState,
   reducers: {
     setAllTicket: (state, action) => {
-      return { ...state, ...action.payload };
+      state.tickets = action.payload;
     },
   },
 });
