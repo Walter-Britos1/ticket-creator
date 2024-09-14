fba760ebbd8533c042e9c340b224a3be180ca1c1
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-14 20:59:26 -0300

    Applying settings to delete ticket

--- client/src/components/TicketItem/TicketItem.jsx
+++ client/src/components/TicketItem/TicketItem.jsx
@@ -1,9 +1,9 @@
-import { Trash2 } from 'lucide-react';
+import DeleteButton from '@/components/DeleteButton/DeleteButton';
+
 export default function TicketItem({ id, name, description, difficulty, status, gifUrl, createdAt, isNew }) {
   return (
     <div
-      className={`bg-white shadow-md rounded-lg p-6 max-w-sm transform transition-all duration-500 ${isNew ? 'scale-100' : 'scale-95 hover:scale-105'
-        }`}
+      className={`bg-white shadow-md rounded-lg p-6 max-w-sm relative transform transition-all duration-500 ${isNew ? 'scale-95 hover:scale-105' : 'scale-100'}`}
     >
       <h2 className='text-xl font-bold mb-2'>{name}</h2>
       <p className='text-gray-700 mb-4'>{description}</p>
@@ -11,9 +11,7 @@ export default function TicketItem({ id, name, description, difficulty, status, 
       <p className='text-sm text-gray-500'>Status:  <span className={`font-semibold ${status === 'resolved' ? 'text-green-500' : 'text-yellow-500'}`}>{status}</span></p>
       <p className='text-sm text-gray-500'>Created At: {new Date(createdAt).toLocaleString()}</p>
       {gifUrl && <img src={gifUrl} alt='Ticket GIF' className='mt-4 rounded-lg' />}
-      <button>
-        <Trash2 className='text-red-500' />
-      </button>
+      <DeleteButton id={id} />
     </div>
   );
 }
--- client/src/components/TicketList/TicketList.jsx
+++ client/src/components/TicketList/TicketList.jsx
@@ -8,20 +8,24 @@ export default function TicketList() {
   useEffect(() => {handlerAllTickets()}, []);
 
   return (
-    <div className='p-4 relative'>
-      <h2 className='text-2xl font-bold mb-4'>Yours Ticket List</h2>
-      {tickets.map((item) => (
-        <TicketItem
-          key={item.id}
-          id={item.id}
-          name={item.name}
-          description={item.description}
-          difficulty={item.difficulty}
-          status={item.status}
-          gifUrl={item.gifUrl}
-          createdAt={item.createdAt}
-        />
-      ))}
+    <div className='p-4'>
+      <h2 className='text-2xl font-bold mb-4 text-center'>Your Ticket List</h2>
+      
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-center'>
+        {tickets.map((item) => (
+          <TicketItem
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            description={item.description}
+            difficulty={item.difficulty}
+            status={item.status}
+            gifUrl={item.gifUrl}
+            createdAt={item.createdAt}
+            isNew={item.isNew} 
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
