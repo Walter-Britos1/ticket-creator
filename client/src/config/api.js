7e30a4c565c118b9e2ec44d248eadc83bb097d17
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-14 20:58:34 -0300

    configuring route to delete ticket

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -3,4 +3,4 @@ export const API_URL_BASE = 'http://localhost:3000/'
 export const GET_ALL_TICKETS_ENDPOINT = API_URL_BASE + 'tickets'
 export const CREATE_TICKET_ENDPOINT = API_URL_BASE + 'ticket'
 export const UPDATE_TICKET_ENDPOINT = API_URL_BASE + 'tickets/:id'
-export const DELETE_TICKET_ENDPOINT = API_URL_BASE + 'ticket/:id'
+export const DELETE_TICKET_ENDPOINT = API_URL_BASE + 'ticket/';
