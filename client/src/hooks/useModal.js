8599a3a39e8c50b92df6f5727f0f38029198d00c
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-14 00:56:01 -0300

    creating hook for form modal

---
+++ client/src/hooks/useModal.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const useModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showModalContent, setShowModalContent] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    setTimeout(() => {
+      setShowModalContent(true);
+    }, 300)
+  };
+
+  const closeModal = () => {
+    setShowModalContent(false);
+    setTimeout(() => {
+      setIsModalOpen(false);
+    }, 300)
+  };
+
+  const handleOutsideClick = (event) => {
+    if (event.target.id === 'modalBackground') {
+      closeModal();
+    }
+  };
+
+  return {
+    isModalOpen,
+    showModalContent,
+    openModal,
+    closeModal,
+    handleOutsideClick,
+  };
+};
+
+export default useModal;
