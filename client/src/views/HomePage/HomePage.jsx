f868286f1c81b1cd89d2270fc1d2ed45ae54cf38
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-16 20:38:00 -0300

    Setting up the home page

--- client/src/views/HomePage/HomePage.jsx
+++ client/src/views/HomePage/HomePage.jsx
@@ -36,7 +36,7 @@ export default function HomePage() {
             >
               X
             </button>
-            <TicketForm />
+             <TicketForm />
           </div>
         </div>
       )}
