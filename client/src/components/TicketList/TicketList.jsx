9c60c7a047a5e372ac3a6f2d84bca6fc7c745379
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-16 00:28:34 -0300

    changing styles for gifs

--- client/src/components/TicketList/TicketList.jsx
+++ client/src/components/TicketList/TicketList.jsx
@@ -9,8 +9,6 @@ export default function TicketList() {
 
   return (
     <div className='p-4'>
-      <h2 className='text-2xl font-bold mb-4 text-center'>Your Ticket List</h2>
-      
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-center'>
         {tickets.map((item) => (
           <TicketItem
