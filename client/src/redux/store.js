9aeb9fcb011f5ae119d0aed117dab783b731b304
Walter-Britos1 <walterbritos418@gmail.com>
2024-09-16 02:31:12 -0300

    Creating states to apply filters

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sliceTicket from './sliceTicket';
+import filtersSlice from './sliceFilters';
 
 export const store = configureStore({
   reducer: {
-    tickets: sliceTicket
-  }
-})
+    tickets: sliceTicket,
+    filterStatus: filtersSlice, 
+  },
+});
