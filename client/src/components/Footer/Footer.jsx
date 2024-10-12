79eecf1b7e08f7c9a7c3afbe4602396b0ea1de0b
Walter-Britos1 <walterbritos418@gmail.com>
2024-10-11 21:49:10 -0300

    comments deleted

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='bg-gray-800 text-white py-4'>
       <div className='container mx-auto flex flex-col md:flex-row justify-between items-center px-4'>
-        {/* Sección de iconos */}
+        
         <div className='flex space-x-4 mb-2 md:mb-0'>
           <a href='https://github.com/Walter-Britos1/' target='_blank' rel='noopener noreferrer'>
             <Github className='w-6 h-6 hover:text-gray-400' />
@@ -14,7 +14,6 @@ export default function Footer() {
           </a>
         </div>
 
-        {/* Sección de texto */}
         <div className='text-sm text-center md:text-left'>
           &copy; {new Date().getFullYear()} Ticket Creator App. Todos los derechos reservados.
         </div>
