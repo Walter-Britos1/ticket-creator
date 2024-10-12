b5a37c466b7f27cc8cb70f38cbc3c47933bf7ea8
Walter Britos <103972809+Walter-Britos1@users.noreply.github.com>
2024-10-12 12:37:47 -0300

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,4 +1,4 @@
-export const API_URL_BASE = 'http://localhost:3000/'
+export const API_URL_BASE = 'https://app-production-5b8b.up.railway.app/'
 
 export const GET_ALL_TICKETS_ENDPOINT = API_URL_BASE + 'tickets'
 export const CREATE_TICKET_ENDPOINT = API_URL_BASE + 'ticket'
